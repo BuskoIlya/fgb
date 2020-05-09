bfa8172b636d5ea535471a5950dd243dd0d24925
BuskoIlya <trassae95st@mail.ru>
2020-05-09 10:39:46 +0300

    [Web] Index page works

---
+++ php/read_news.php
@@ -0,0 +1,10 @@
+<?php
+$year = trim(filter_var($_POST['year'], FILTER_SANITIZE_STRING));
+
+require_once 'fgb_db_connect.php';
+$sql = 'SELECT * FROM `news` WHERE `year` = :year';
+$query = $pdo->prepare($sql);
+$query->execute(['year' => $year]);
+$tournaments = $query->fetchAll(PDO::FETCH_OBJ);
+echo json_encode($tournaments);
+?>
