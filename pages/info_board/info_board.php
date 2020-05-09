c5b3bf18bf5ebb92e4a3b4c4e837fe18fa7f1b76
BuskoIlya <trassae95st@mail.ru>
2020-05-10 01:03:16 +0300

    [Web] Add info board page

---
+++ pages/info_board/info_board.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+    <?php
+    require '../blocks/set_lang.php';
+    require_once '../../php/lang/lang_' . $_SESSION['lang'] . '.php';
+    $website_title = $Lang['page_players'];
+    require '../blocks/head_pages.php';
+    ?>
+</head>
+<body>
+<div id="fgb_body">
+    <?php
+    require '../blocks/top_pages.php';
+    require '../blocks/menu_pages.php';
+    ?>
+
+    <div id="info_board_id"/>
+
+    <?php
+    require '../blocks/bottom.php';
+    ?>
+</div>
+</body>
+
+<script>
+    $(document).ready(function () {
+        var lang = "<?php echo $_SESSION['lang'] ?>";
+        if (lang == 'ru') {
+            $("#info_board_id").load("info_board_ru.html");
+        } else {
+            $("#info_board_id").load("info_board_en.html");
+        }
+    });
+</script>
