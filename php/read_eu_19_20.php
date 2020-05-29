14f132d7657436d2df8627d92351adc1fa93c7f0
BuskoIlya <trassae95st@mail.ru>
2020-05-29 18:30:49 +0300

    [Web] Final result of groups competitions

--- php/read_eu_19_20.php
+++ php/read_eu_19_20.php
@@ -1,7 +1,7 @@
 <?php
 $eu_group = trim(filter_var($_POST['eu_group'], FILTER_SANITIZE_STRING));
 require_once 'fgb_db_connect.php';
-$sql = 'SELECT * FROM `eu_19_20` WHERE `eu_group` = :eu_group ORDER BY `score` DESC';
+$sql = 'SELECT * FROM `eu_19_20` WHERE `eu_group` = :eu_group ORDER BY `score` DESC, `bw` DESC, `b1` DESC, `b2` DESC';
 $query = $pdo->prepare($sql);
 $query->execute(['eu_group' => $eu_group]);
 $eu_19_20 = $query->fetchAll(PDO::FETCH_OBJ);
